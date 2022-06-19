7c9fcbf3a221cb8313f38ee14125df0e8d2c891c
Zhicheng WANG <zcwang@thoughtworks.com>
2022-06-19 19:54:47 +0800

    feat(lib): WIP - define models

--- projects/model-driven-graph/src/lib/models/graph.ts
+++ projects/model-driven-graph/src/lib/models/graph.ts
@@ -1,2 +1,23 @@
+import { Edge } from './edge';
+import { Vertex } from './vertex';
+
 export class Graph {
+  get vertexes(): readonly Vertex[] {
+    return this._vertexes;
+  }
+
+  set vertexes(value: readonly Vertex[]) {
+    this._vertexes = [...value];
+  }
+
+  get edges(): readonly Edge[] {
+    return this._edges;
+  }
+
+  set edges(value: readonly Edge[]) {
+    this._edges = [...value];
+  }
+
+  private _vertexes: Vertex[] = [];
+  private _edges: Edge[] = [];
 }
